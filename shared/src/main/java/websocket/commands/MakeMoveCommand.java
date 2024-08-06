09201cec87faac1a2244ca19f5b04a43926cb37f
ethan_bang <ebang1919@gmail.com>
2024-08-06 15:20:31 -0600

    Initialized the rest of the Commands/Messages classes. Initialized WebSocketHandler and WebSocketService. Phase 6

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -3,7 +3,9 @@ package websocket.commands;
 import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand{
+    ChessMove move;
     public MakeMoveCommand(CommandType type, String token, Integer id, ChessMove move) {
         super(type, token, id);
+        this.move = move;
     }
 }
---
+++ shared/src/main/java/websocket/commands/ResignCommand.java
@@ -0,0 +1,7 @@
+package websocket.commands;
+
+public class ResignCommand extends UserGameCommand{
+    public ResignCommand(CommandType type, String token, Integer id) {
+        super(type, token, id);
+    }
+}
