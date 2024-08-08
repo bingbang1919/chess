5ab7837e7f5a860aea5312f97383d5bd14823691
ethan_bang <ebang1919@gmail.com>
2024-08-08 16:20:06 -0600

    Successfully reimplemented a bare-bones Gameplay REPL. Phase 6

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,13 +1,18 @@
+import chess.ChessBoard;
 import ui.ChessClient;
+import ui.GameplayREPL;
+import ui.NewerGameplayREPL;
 import ui.PreloginREPL;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var serverUrl = "http://localhost:7389";
-        if (args.length == 1) {
-            serverUrl = args[0];
-        }
-        new PreloginREPL(serverUrl, new ChessClient(serverUrl)).run();
+//        var serverUrl = "http://localhost:7389";
+//        if (args.length == 1) {
+//            serverUrl = args[0];
+//        }
+//        new PreloginREPL(serverUrl, new ChessClient(serverUrl)).run();
+        new NewerGameplayREPL().run(new ChessBoard(), true);
+        new NewerGameplayREPL().run(new ChessBoard(), false);
     }
 
 }
--- client/src/main/java/ui/NewerGameplayREPL.java
+++ client/src/main/java/ui/NewerGameplayREPL.java
@@ -3,6 +3,7 @@ package ui;
 import chess.ChessBoard;
 
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 import static ui.EscapeSequences.*;
 
@@ -11,21 +12,46 @@ public class NewerGameplayREPL {
 
     public void run(ChessBoard board, boolean whiteView) {
         String[] headers = { "   "," A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", "   " };
-        drawHeader(headers, whiteView);
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        drawHeader(out, headers, whiteView);
+        drawBoard(out, board, whiteView);
+        drawHeader(out, headers, whiteView);
 
-        drawHeader(headers, whiteView);
     }
 
-    private void drawBoard(ChessBoard board, boolean whiteView) {
+    private void drawBoard(PrintStream out, ChessBoard board, boolean whiteView) {
         String[] rowMarkers = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 " };
-        for (int row = 0; row< CHESS_BOARD_SIZE+2; row++) {
-            
+        if (whiteView) {
+            for (int i = CHESS_BOARD_SIZE-1; i >=0 ; i--) {
+                out.print(rowMarkers[i]);
+                for (int j=0; j<CHESS_BOARD_SIZE; j++) {
+                    out.print(" H ");
+                }
+                out.println(rowMarkers[i]);
+            }
+        } else {
+            for (int i = 0; i < CHESS_BOARD_SIZE ; i++) {
+                out.print(rowMarkers[i]);
+                for (int j= CHESS_BOARD_SIZE-1; j>=0; j--) {
+                    out.print(" H ");
+                }
+                out.println(rowMarkers[i]);
+            }
         }
     }
 
-    private void drawHeader(String[] headers, boolean whiteView) {
+    private void drawHeader(PrintStream out, String[] headers, boolean whiteView) {
+        String[] columnMarkers = {"   ", " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", "   "};
         if (whiteView) {
-
+            for (int i=0; i<CHESS_BOARD_SIZE+2; i++) {
+                out.print(columnMarkers[i]);
+            }
+            out.println();
+        } else {
+            for (int i=CHESS_BOARD_SIZE+1; i>=0; i--) {
+                out.print(columnMarkers[i]);
+            }
+            out.println();
         }
     }
 
@@ -34,4 +60,7 @@ public class NewerGameplayREPL {
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
+    private void printPosition(PrintStream out, int row, int col) {
+
+    }
 }
